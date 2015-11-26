1ea7450ce50f4f1b7eca9203d46494c7c47a6830
Daniel <dm0275@gmail.com>
2015-11-25 21:48:08 -0600

    Created a frame and set the background color

--- src/CalculatorFrame.java
+++ src/CalculatorFrame.java
@@ -1,7 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by daniel on 11/25/15.
  */
-public class CalculatorFrame
+public class CalculatorFrame extends JFrame
 {
+    private JPanel panel = new JPanel();
+    private JTextField field = new JTextField("This is a text field!");
+
+    public CalculatorFrame(String calculator)
+    {
+        this.setTitle(calculator);
+        this.setVisible(true);
+        this.setSize(500,500);
+        //this.getContentPane().setBackground(Color.BLACK);
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+        panel.setBackground(Color.BLACK);
+        panel.add(field);
+        this.add(panel);
+    }
 
 }
